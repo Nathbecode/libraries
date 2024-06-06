09924315d0c44de73384ce7f206b85dbaa28ceda
Nathaly V <166789478+Nathbecode@users.noreply.github.com>
2024-06-06 11:31:05 +0200

    changes

--- waypoints/app.js
+++ waypoints/app.js
@@ -1,7 +1,19 @@
-var waypoint = new Waypoint({
-    element: document.getElementById('basic-waypoint'),
-    handler: function() {
-      notify('Basic waypoint triggered')
+document.addEventListener('DOMContentLoaded', function() {
+    var waypointElement = document.getElementById('basic-waypoint');
+    if (waypointElement) {
+        var waypoint = new Waypoint({
+            element: waypointElement,
+            handler: function(direction) {
+                if (direction === 'down') {
+                    alert('Are you sure? Things are wild down there.');
+                } else if (direction === 'up') {
+                    alert('i told you')
+                }
+            },
+            offset: '100%' // Trigger when the top of the element is at the bottom of the viewport
+        });
+    } else {
+        console.error('Waypoint element not found');
     }
-  })
+});
 
